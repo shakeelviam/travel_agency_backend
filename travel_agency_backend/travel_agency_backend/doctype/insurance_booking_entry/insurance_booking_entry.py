1a784eb45244132e133f6db8694bf1f2ce4e0c1c
shakeelviam <shakeel.viam@gmail.com>
2025-06-01 09:14:16 +0000

    feat: add Insurance Booking Entry child Doctype

---
+++ travel_agency_backend/travel_agency_backend/doctype/insurance_booking_entry/insurance_booking_entry.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Shakeel Mohammed Viam and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class InsuranceBookingEntry(Document):
+	pass
