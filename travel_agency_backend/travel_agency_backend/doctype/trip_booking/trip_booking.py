99a2394ebd1a4f663687448091ecbcde5494f0e9
shakeelviam <shakeel.viam@gmail.com>
2025-06-04 18:13:13 +0300

    fix: restore working Trip Booking code with get_all_booking_tables

--- travel_agency_backend/travel_agency_backend/doctype/trip_booking/trip_booking.py
+++ travel_agency_backend/travel_agency_backend/doctype/trip_booking/trip_booking.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2023, Shakeel Viam and contributors
+# For license information, please see license.txt
+
 import frappe
 from frappe.model.document import Document
 from frappe.utils import now_datetime
