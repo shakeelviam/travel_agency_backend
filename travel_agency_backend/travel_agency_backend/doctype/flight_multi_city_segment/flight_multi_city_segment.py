680a1eead250027acaf2185951d5fc12645dfe03
shakeelviam <shakeel.viam@gmail.com>
2025-07-22 11:22:27 +0300

    Remove all multi-city related code and DocTypes for clean codebase

--- travel_agency_backend/travel_agency_backend/doctype/flight_multi_city_segment/flight_multi_city_segment.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2025, Shakeel Viam and contributors
-# For license information, please see license.txt
-
-import frappe
-from frappe.model.document import Document
-
-class FlightMultiCitySegment(Document):
-	pass
--- travel_agency_backend/travel_agency_backend/multi_city_test/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2025, Shakeel Mohammed Viam and contributors
-# For license information, please see license.txt
